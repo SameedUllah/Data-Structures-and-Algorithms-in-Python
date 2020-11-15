9dd6b23c26888922d47922818857ee95a29d4270
sameedullah <sameedu786@gmail.com>
2020-11-15 18:32:53 +0500

    Recursion.py

--- Recursion.py
+++ Recursion.py
@@ -1,6 +1,10 @@
 # Factorial
+import sys
+sys.setrecursionlimit(10000)
 
-fact = 10
+x = 3
 
-def factorial(x):
-    factorial(x-1)
+def factorial(n):
+    print(n)
+    return n * factorial(n-1)
+factorial(x)
